3b669101503df44d576757931a752f393d6b5cac
M0HAX WeazelNews <95334628+M0HAXXX@users.noreply.github.com>
2022-05-05 15:08:45 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int weight = scanner.nextInt();
+    if ( weight % 2 == 0 )
+      System.out.println("YES");
+    else
+      System.out.println("NO");
+  }
+}
